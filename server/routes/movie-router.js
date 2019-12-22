2d64e058d6ca1c212bafe5b7b20f190e21239243
Phillip Berg <phillipb1991@msn.com>
2019-12-22 16:46:18 -0600

    set up movie detail get route

--- server/routes/movie-router.js
+++ server/routes/movie-router.js
@@ -3,12 +3,36 @@ const pool = require('./../modules/pool');
 
 const router = express.Router();
 
-router.get('/',(req,res)=>{
+// Get all movies from DB
+router.get('/', (req,res)=>{
   const queryText = `SELECT * FROM "movies" ORDER BY "title";`;
   pool.query(queryText).then( results => {
     res.send(results.rows);
   }).catch(err=>{
     console.log('Error in get movies', err);
+    res.sendStatus(500);
+  });
+});
+
+// GET specific movie from DB
+router.get('/:name', (req,res)=>{
+  const queryText = 
+      `SELECT 
+        "movies"."title", 
+        "movies"."poster", 
+        "movies"."description", 
+        array_agg("genres"."name") as "genres"
+      FROM "movies" 
+      LEFT JOIN "m_g" ON "movies"."id" = "m_g"."mov_id"
+      LEFT JOIN "genres" ON "genres"."id" = "m_g"."gen_id"
+      WHERE "movies"."title" = $1
+      GROUP BY "movies"."id"
+      ORDER BY "movies"."title";`;
+  pool.query(queryText, [req.params.name]).then( results => {
+    res.send(results.rows);
+  }).catch(err=>{
+    console.log('Error in get movies', err);
+    res.sendStatus(500);
   });
 });
 
