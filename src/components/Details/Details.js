18bc6efc798f0d874a47a4c010f4e56996d073a1
Phillip Berg <phillipb1991@msn.com>
2019-12-22 17:01:28 -0600

    set up Details Comonent

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Details = (props) => {
+  // Adds dispatch
+  const dispatch = useDispatch();
+
+  // Gets specific movie info from DB and puts it on movie reducer
+  useEffect(()=>{
+    dispatch({type: 'GET_MOVIE_INFO', payload: props.match.params.name})
+  }, [dispatch, props.match.params.name]);
+  
+  // Gets movie info from reducer
+  const movieInfo = useSelector(state => state.movieInfo);
 
   return (
     <div>
-      {JSON.stringify(props)}
+      <img 
+        src={'/'+movieInfo.poster} //unsure why / is needed
+        alt={movieInfo.title}
+      />
+      <h1>{movieInfo.title}</h1>
+      <p>{movieInfo.description}</p>
     </div>
   );
 };
