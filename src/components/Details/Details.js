2adf6dfbc96bebc31080a0c333433af25e2cdfeb
Phillip Berg <phillipb1991@msn.com>
2019-12-22 17:23:52 -0600

    added back to list button

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,9 +1,13 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 const Details = (props) => {
   // Adds dispatch
   const dispatch = useDispatch();
+
+  // Adds history for progamatic routing
+  let history = useHistory();
 
   // Gets specific movie info from DB and puts it on movie reducer
   useEffect(()=>{
@@ -21,6 +25,7 @@ const Details = (props) => {
       />
       <h1>{movieInfo.title}</h1>
       <p>{movieInfo.description}</p>
+      <button onClick={()=>history.push('')}>Back to List</button>
     </div>
   );
 };
