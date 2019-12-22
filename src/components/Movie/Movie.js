143b28a60c5109bc1dfe7b61e30bbc99dacb56ee
Phillip Berg <phillipb1991@msn.com>
2019-12-22 16:13:21 -0600

    clicking on poster pushes to movie details page

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,9 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 const Movie = (props) => {
+  // Adds history for progamatic routing
+  let history = useHistory();
+
   return(
     <div key={props.movie.id}>
-      <img src={props.movie.poster}/>
+      <img 
+        src={props.movie.poster} 
+        onClick={()=>history.push(`/details/${props.movie.title}`)}
+        alt={props.movie.title}
+      />
       <h1>{props.movie.title}</h1>
       <p>{props.movie.description}</p>
     </div>
