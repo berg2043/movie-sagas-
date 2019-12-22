8caf6287ba90164f60bca9c93e256cae1ac39ed3
Phillip Berg <phillipb1991@msn.com>
2019-12-22 16:51:36 -0600

    fixed get request

--- src/sagas/get-movie-info/index.js
+++ src/sagas/get-movie-info/index.js
@@ -3,7 +3,7 @@ import { put } from 'redux-saga/effects'
 
 function* getMovies (action){
   try {
-    const response = yield Axios.get('/api/movies/:name')
+    const response = yield Axios.get('/api/movies/' + action.payload)
     yield put({type: 'SET_MOVIE_INFO', payload: response.data[0]})
   } catch (error) {
     console.log('Error getting movie info',error);
