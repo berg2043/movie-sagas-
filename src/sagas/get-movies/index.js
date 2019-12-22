87ee4b4f8df1c30538b115ac13cfcfeae5a3d75f
Phillip Berg <phillipb1991@msn.com>
2019-12-22 15:55:02 -0600

    declared variable

--- src/sagas/get-movies/index.js
+++ src/sagas/get-movies/index.js
@@ -3,7 +3,7 @@ import { put } from 'redux-saga/effects'
 
 function* getMovies (action){
   try {
-    response = yield Axios.get('/api/movies')
+    const response = yield Axios.get('/api/movies')
     yield put({type: 'SET_MOVIES', payload: response.data})
   } catch (error) {
     console.log('Error getting movies',error);
