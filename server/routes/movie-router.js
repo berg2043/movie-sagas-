84a17df7d1f307a32df9e37d638e9a734f07ffbd
Phillip Berg <phillipb1991@msn.com>
2019-12-22 18:04:45 -0600

    added Put route

--- server/routes/movie-router.js
+++ server/routes/movie-router.js
@@ -37,4 +37,19 @@ router.get('/:name', (req,res)=>{
   });
 });
 
+// Update a movie
+router.put('/', (req,res)=>{
+  const queryText = 
+      `UPDATE "movies" 
+      SET title = $1, description = $2 
+      WHERE "id" = $3;`;
+  const queryParams = [req.body.title, req.body.description, req.body.id]
+  pool.query(queryText, queryParams).then( results => {
+    res.sendStatus(200);
+  }).catch(err=>{
+    console.log('Error in get movies', err);
+    res.sendStatus(500);
+  });
+});
+
 module.exports = router;
