8bfc5b3b6201d49642f318dc147fb82150a9a409
Phillip Berg <phillipb1991@msn.com>
2019-12-22 22:00:22 -0600

    set up grid and buttons

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,8 +1,31 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import {Grid, Button, List, ListItem, ListItemText} from '@material-ui/core'
+
+const useStyles = makeStyles(theme => ({
+  root:{
+    backgroundColor: 'gray',
+    maxWidth: 800,
+    margin:'auto'
+  },
+  image:{
+    maxWidth: 1,
+    margin:'auto'
+  },
+  content:{
+    maxWidth: 800
+  },
+  button: {
+    margin: theme.spacing(1),
+  }
+}));
 
 const Details = (props) => {
+  
+  const classes = useStyles();
+  
   // Adds dispatch
   const dispatch = useDispatch();
 
@@ -18,20 +41,47 @@ const Details = (props) => {
   const movieInfo = useSelector(state => state.movieInfo);
 
   return (
-    <div>
-      <img 
-        src={'/'+movieInfo.poster} //unsure why / is needed
-        alt={movieInfo.title}
-      />
-      <h1>{movieInfo.title}</h1>
-      <p>{movieInfo.description}</p>
-      <h2>Genres</h2>
-      <ul>
-        <p>{movieInfo.genres.map((genre, i)=>{return(<li key={i}>{genre}</li>)})}</p>
-      </ul>
-      <button onClick={()=>history.push('/')}>Back to List</button>
-      <button onClick={()=>history.push('/edit/'+movieInfo.title)}>Edit</button>
-    </div>
+    <Grid container className={classes.root}>
+      <Grid item xs={6} className={classes.image}>
+        <img 
+          src={'/'+movieInfo.poster} //unsure why / is needed
+          alt={movieInfo.title}
+        />
+      </Grid>
+      <Grid item xs={6}>
+        <h2>Genres</h2>
+        <List>
+          {movieInfo.genres.map((genre, i)=>{
+            return(
+              <ListItem key={i}>
+                <ListItemText primary={genre}/>
+              </ListItem>
+              )})}
+        </List>
+      </Grid>
+      <Grid item className={classes.content} xs={12}>
+        <h1>{movieInfo.title}</h1>
+        <p>{movieInfo.description}</p>
+      </Grid>
+      <Grid item xs={12}>
+        <Button 
+          className={classes.button}
+          variant="contained" 
+          color="secondary" 
+          onClick={()=>history.push('/')}
+        >
+          Back to List
+        </Button>
+        <Button 
+          className={classes.button}
+          variant="contained" 
+          color="primary" 
+          onClick={()=>history.push('/edit/'+movieInfo.title)}
+        >
+          Edit
+        </Button>
+      </Grid>
+    </Grid>
   );
 };
 
