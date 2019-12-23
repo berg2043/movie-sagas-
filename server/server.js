85443139bdb70b152e3cc5c0cb7aed12844a3de4
Phillip Berg <phillipb1991@msn.com>
2019-12-22 20:44:21 -0600

    added genre Route

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const movieRouter = require('./routes/movie-router')
+const genreRouter = require('./routes/genre-router')
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(express.json());
@@ -10,6 +11,7 @@ app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
 app.use('/api/movies', movieRouter);
+app.use('/api/genres', genreRouter)
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
