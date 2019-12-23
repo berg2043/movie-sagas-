34ba05bcd81770da981dc3591492e7a1c188589c
Phillip Berg <phillipb1991@msn.com>
2019-12-22 20:46:09 -0600

    updated route to be able to update genres

--- server/routes/movie-router.js
+++ server/routes/movie-router.js
@@ -45,7 +45,29 @@ router.put('/', (req,res)=>{
       WHERE "id" = $3;`;
   const queryParams = [req.body.title, req.body.description, req.body.id]
   pool.query(queryText, queryParams).then( results => {
-    res.sendStatus(200);
+    const deleteText = `DELETE FROM "m_g" WHERE "mov_id" = $1;`;
+    pool.query(deleteText, [req.body.id]).then( result => {
+      let count = 2;
+      let holderStr = '';
+      let holderArr = [];
+      for(genre in req.body.genres){ // Loops through genres to see if any are true
+        if(req.body.genres[genre].checked){
+          holderStr += (`($1, $${count})`);
+          holderArr.push(req.body.genres[genre].id);
+          count ++;
+        }
+      }
+      if(holderArr.length){ // if any of the genres are true, it creates a query to add them
+        holderArr.unshift(req.body.id)
+        holderStr = holderStr.split(')(');
+        holderStr = holderStr.join('), (');
+        let holderQuery = 'INSERT INTO "m_g" ("mov_id", "gen_id") VALUES ' + holderStr +';';
+        pool.query(holderQuery, holderArr);
+        res.sendStatus(200);
+      } else {
+        res.sendStatus(200);
+      }
+    });
   }).catch(err=>{
     console.log('Error in get movies', err);
     res.sendStatus(500);
