6c2b42874f6e02932b889b5842e25998a74223cb
Phillip Berg <phillipb1991@msn.com>
2019-12-22 22:00:12 -0600

    set up grid with cards

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,20 +1,43 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import {Card, CardMedia, CardContent, CardHeader} from '@material-ui/core'
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    maxWidth: 400,
+    backgroundColor: 'lightgray',
+    margin: 'auto'
+  },
+  media: {
+    width: '50%',
+    marginTop:'0',
+    marginLeft:'auto',
+    marginRight:'auto'
+  },
+}));
 
 const Movie = (props) => {
+  
+  const classes = useStyles();
+  
   // Adds history for progamatic routing
   let history = useHistory();
 
   return(
-    <div key={props.movie.id}>
-      <img 
-        src={props.movie.poster} 
+    <Card key={props.movie.id} className={classes.card}>
+      <CardHeader title={props.movie.title}/>
+      <CardMedia 
+        className={classes.media}
+        component="img"
+        image={props.movie.poster} 
         onClick={()=>history.push(`/details/${props.movie.title}`)}
-        alt={props.movie.title}
+        title={props.movie.title}
       />
-      <h1>{props.movie.title}</h1>
-      <p>{props.movie.description}</p>
-    </div>
+      <CardContent>
+        <p>{props.movie.description}</p>
+      </CardContent>
+    </Card>
   );
 };
 
--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,23 +1,43 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Movie from '../Movie/Movie';
+import { makeStyles } from '@material-ui/core/styles';
 import {Grid} from '@material-ui/core'
 
+const useStyles = makeStyles(theme => ({
+  root:{
+    backgroundColor: 'gray',
+  }
+}));
 
 const MovieList = (props) => {
+  
+  const classes = useStyles();
+
   // Adds movies reducer
   const movieList = useSelector(state => state.movies);
 
   return (
-    <Grid container>
-      {movieList.map(movie=>{
-        return(
-          <Grid item key={movie.id} xs={12} md={6} lg={4}>
-            <Movie movie={movie}/>
-          </Grid>
-        )
-      })}
-    </Grid>
+    <>
+      <header>
+        <h1>Movie List</h1>
+      </header>
+      <Grid 
+        container 
+        spacing={3} 
+        className={classes.root} 
+        direction="row"
+        justify="center"
+      >
+        {movieList.map(movie=>{
+          return(
+            <Grid item key={movie.id} xs={12} md={6} lg={4}>
+              <Movie movie={movie}/>
+            </Grid>
+          )
+        })}
+      </Grid>
+    </>
   );
 };
 
