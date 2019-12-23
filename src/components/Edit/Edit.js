1720d3ccb109fe7c38b8f9e9c298acac8ef1ae5e
Phillip Berg <phillipb1991@msn.com>
2019-12-22 20:45:58 -0600

    added ability to eddit genres

--- src/components/Edit/Edit.js
+++ src/components/Edit/Edit.js
@@ -6,7 +6,8 @@ const Edit = (props) => {
   // State values
   const [title, setTitle] = useState('')
   const [description, setDescription] = useState('')
-  
+  const [genres, setGenres] = useState({})
+
   // Adds dispatch
   const dispatch = useDispatch();
 
@@ -20,7 +21,30 @@ const Edit = (props) => {
 
   // Gets movie info from reducer
   const movieInfo = useSelector(state => state.movieInfo);
+  
+  // Gets genres from reducer
+  const genreList = useSelector(state => state.genres);
 
+  // Sets up default genre state
+  useEffect(()=>{
+    const holder = {}
+    for(let genre of genreList){
+      const value = movieInfo.genres.includes(genre.name) // Marks genres already there
+      holder[genre.name] = {id: genre.id, checked: value};
+    }
+    setGenres(holder)
+  },[genreList, movieInfo.genres])
+
+  // Handles the changes to checkboxes
+  function checkboxes(event){
+    const value = event.target.checked;
+    setGenres({
+      ...genres,
+      [event.target.name]: {
+        id: genres[event.target.name].id,
+        checked: value
+      }})
+  }
   // Updates state after movie info reducer is updated
   useEffect(()=>{
     setTitle(movieInfo.title)
@@ -33,7 +57,8 @@ const Edit = (props) => {
     const payload = {
       id: movieInfo.id,
       title: title,
-      description: description
+      description: description,
+      genres: genres
     }
     dispatch({type: 'PUT_MOVIE', payload: payload})
     history.push('/details/'+title)
@@ -59,6 +84,19 @@ const Edit = (props) => {
           value={description}
           onChange={(event)=>setDescription(event.target.value)}
         />
+        {Object.entries(genres).map((genre, i)=>{
+          return(
+            <span key={i}>
+              <input
+                type="checkbox"
+                name={genre[0]} 
+                checked={genres[genre[0]].checked}
+                onChange={checkboxes}
+              /> 
+              <span>{genre[0]}</span>
+            </span>
+          )
+        })}
         <button type="button" onClick={cancel}>Cancel</button>
         <button type="submit" onClick={saveInput}>Save</button>
       </form>
